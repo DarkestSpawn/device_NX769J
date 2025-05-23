5e38c7d71be1cbfb23143d523155904ccc44a745
azzzddddd <105015339+azzzddddd@users.noreply.github.com>
2025-05-24 05:35:35 +0800

    Update twrp_NX769J.mk

--- twrp_NX769J.mk
+++ twrp_NX769J.mk
@@ -4,7 +4,7 @@
 # SPDX-License-Identifier: Apache-2.0
 #
 
-DEVICE_PATH := device/nubia/NX769J
+DEVICE_PATH := device/Nubia/NX769J
 
 # Inherit from device.mk configuration
 $(call inherit-product, $(DEVICE_PATH)/device.mk)
